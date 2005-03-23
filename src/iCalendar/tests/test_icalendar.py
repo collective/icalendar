b552e61d36346d3cc355ebacda8875c840baf972
faassen <faassen@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2005-03-23 14:27:37 +0000

    Convert .py files to .txt files for examples, using new doctest.py. Add them to test_icalendar.py, so that they get picked up by the testrunner.
    git-svn-id: http://codespeak.net/svn/iCalendar/trunk@10117 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- src/iCalendar/tests/test_icalendar.py
+++ src/iCalendar/tests/test_icalendar.py
@@ -1,14 +1,17 @@
-import unittest, doctest
+import unittest, doctest, os
 from iCalendar import iCalendar, CaselessDict, ContentlinesParser
 from iCalendar import PropertyValues, tools
 
 def test_suite():
     suite = unittest.TestSuite()
+    
     suite.addTest(doctest.DocTestSuite(CaselessDict))
     suite.addTest(doctest.DocTestSuite(ContentlinesParser))
     suite.addTest(doctest.DocTestSuite(PropertyValues))
     suite.addTest(doctest.DocTestSuite(iCalendar))
-
+    doc_dir = '../../../doc'
+    for docfile in ['example.txt', 'groupscheduled.txt', 'small.txt']:
+        suite.addTest(doctest.DocFileSuite(os.path.join(doc_dir, docfile)))
     # only has a disabled doctest
     # suite.addTest(doctest.DocTestSuite(tools))
     return suite
