05db355efc88f58a053ba66eff56232d34ff4a29
Lennart Regebro <regebro@gmail.com>
2005-04-04 16:47:58 +0000

    Distutils file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='icalendar',
+      version='0.10',
+      description='iCalendar support module',
+      package_dir = {'': 'src'},
+      packages=['icalendar'],
+     )
