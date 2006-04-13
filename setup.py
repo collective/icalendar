298129c2fb2697f2227fa156f44d3fc14ae3aabc
Lennart Regebro <regebro@gmail.com>
2006-04-13 16:17:21 +0000

    Added support for creating python eggs.

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-f = open('version.txt', 'r')
-version = f.read().strip()
-f.close()
+version = file('version.txt').read().strip()
 
 setup(name='icalendar',
-      version=version,
-      description='iCalendar support module',
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       packages=['icalendar'],
-     )
+      version=version,
+
+      # metadata for upload to PyPI
+      author='MaxM',
+      author_email='maxm@mxm.dk',
+      description='iCalendar parser/generator',
+      license='GPL2.1',
+      keywords='calendar icalendar',
+      url='http://codespeak.net/icalendar/',
+      long_description="""iCalendar is a parser/generator of iCalendar files 
+          (RFC 2445) for use with Python.""",
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Operating System :: OS Independent'],
+      platforms='All',
+      )
