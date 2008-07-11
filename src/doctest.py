39548a34085bc8426254dee5c41757804142c347
regebro <regebro@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2008-07-11 17:27:05 +0000

    Run doctests also under 2.5/2.6.
    git-svn-id: http://codespeak.net/svn/iCalendar/trunk@56462 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- src/doctest.py
+++ src/doctest.py
@@ -1332,7 +1332,7 @@ class DocTestRunner:
     __LINECACHE_FILENAME_RE = re.compile(r'<doctest '
                                          r'(?P<name>[\w\.]+)'
                                          r'\[(?P<examplenum>\d+)\]>$')
-    def __patched_linecache_getlines(self, filename):
+    def __patched_linecache_getlines(self, filename, module_globals=None):
         m = self.__LINECACHE_FILENAME_RE.match(filename)
         if m and m.group('name') == self.test.name:
             example = self.test.examples[int(m.group('examplenum'))]
