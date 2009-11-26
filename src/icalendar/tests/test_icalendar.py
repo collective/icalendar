2394e7c8eb35870745d78e816b9145e7c07917c8
Wichert Akkerman <wichert@wiggy.net>
2009-11-26 18:21:54 +0000

    Add changelog entry

--- src/icalendar/tests/test_icalendar.py
+++ src/icalendar/tests/test_icalendar.py
@@ -1,7 +1,41 @@
 import unittest, doctest, os
 from icalendar import cal, caselessdict, parser, prop
 
+class FuckYouTests(unittest.TestCase):
+    def XtestBla(self):
+        from icalendar import Calendar
+        c = Calendar()
+        c['description']=u'Paragraph one\n\nParagraph two'
+        output = c.as_string()
+        self.assertEqual(output, 
+                "BEGIN:VCALENDAR\r\nDESCRIPTION:Paragraph one\r\n \r\n Paragraph two\r\nEND:VCALENDAR\r\n")
+
+    def XtestTrailingNewline(self):
+        from icalendar.parser import Contentlines, Contentline
+        c = Contentlines([Contentline('BEGIN:VEVENT\\r\\n')])
+        output = str(c)
+        self.assertEqual(output, 'BEGIN:VEVENT\\r\\n')
+
+    def XtestLongLine(self):
+        from icalendar.parser import Contentlines, Contentline
+        c = Contentlines([Contentline('BEGIN:VEVENT\\r\\n')])
+        c.append(Contentline(''.join(['123456789 ']*10)+'\\r\\n'))
+        import pdb ; pdb.set_trace()
+        output = str(c)
+        self.assertEqual(output, 
+                "BEGIN:VEVENT\\r\\n\\r\\n123456789 123456789 123456789 123456789 123456789 123456789 123456789 1234\\r\\n 56789 123456789 123456789 \\r\\n")
+
+    def testHmm(self):
+        from icalendar.parser import Contentlines, Contentline
+        c = Contentlines([Contentline('BEGIN:VEVENT\r\n')])
+        c.append(Contentline(''.join(['123456789 ']*10)+'\r\n'))
+        output = str(c)
+        self.assertEqual(output, 
+                'BEGIN:VEVENT\r\n\r\n123456789 123456789 123456789 123456789 123456789 123456789 123456789 1234\r\n 56789 123456789 123456789 \r\n')
+
 def test_suite():
+#    return unittest.defaultTestLoader.loadTestsFromName(__name__)
+
     suite = unittest.TestSuite()
 
     suite.addTest(doctest.DocTestSuite(caselessdict))
