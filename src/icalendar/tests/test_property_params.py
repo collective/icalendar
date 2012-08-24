614228995643c10be71924aa2ab2cd34ed327a5d
Johannes Raggam <raggam-nl@adm.at>
2012-08-24 14:31:16 +0200

    test also that property parameters are build from an icalendar string

--- src/icalendar/tests/test_property_params.py
+++ src/icalendar/tests/test_property_params.py
@@ -10,8 +10,12 @@ class TestPropertyParams(unittest.TestCase):
         ical.add('organizer', cal_address)
 
         ical_str = icalendar.Calendar.to_ical(ical)
-        exp_str = """BEGIN:VCALENDAR\r\nORGANIZER;CN="Doe, John":mailto:john.doe@example.org\r\nEND:VCALENDAR\r\n"""
+        exp_str = """BEGIN:VCALENDAR\r\nORGANIZER;CN="Doe, John":"""\
+                  """mailto:john.doe@example.org\r\nEND:VCALENDAR\r\n"""
 
         self.assertEqual(ical_str, exp_str)
 
-        raise Exception
+        # other way around: ensure the property parameters can be restored from
+        # an icalendar string.
+        ical2 = icalendar.Calendar.from_ical(ical_str)
+        self.assertEqual(ical2.get('ORGANIZER').params.get('CN'), 'Doe, John')
