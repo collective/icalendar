a7b2fab84863d38fa23a5fbea390490b11fe5cc4
Johannes Raggam <raggam-nl@adm.at>
2013-07-19 11:27:23 +0200

    test for multiple exdate lines

--- src/icalendar/tests/test_recurrence.py
+++ src/icalendar/tests/test_recurrence.py
@@ -10,13 +10,14 @@ from . import unittest
 
 class TestRecurrence(unittest.TestCase):
 
-    def test_recurrence_create_from_ical(self):
+    def setUp(self):
         directory = os.path.dirname(__file__)
-        cal = icalendar.Calendar.from_ical(
-                open(os.path.join(directory, 'recurrence.ics'), 'rb').read()
+        self.cal = icalendar.Calendar.from_ical(
+            open(os.path.join(directory, 'recurrence.ics'), 'rb').read()
         )
 
-        first_event = cal.walk('vevent')[0]
+    def test_recurrence_exdates_one_line(self):
+        first_event = self.cal.walk('vevent')[0]
 
         self.assertEqual(
             str(first_event['rrule']),
@@ -42,3 +43,20 @@ class TestRecurrence(unittest.TestCase):
             first_event['exdate'].dts[2].dt,
             datetime.datetime(1996, 4, 4, 1, 0, tzinfo=pytz.utc)
         )
+
+    def test_recurrence_exdates_multiple_lines(self):
+        event = self.cal.walk('vevent')[1]
+
+        exdate = event['exdate']
+
+        # TODO: DOCUMENT BETTER!
+        # In this case we have multiple EXDATE definitions, one per line.
+        # Icalendar makes a list out of this instead of zipping it into one
+        # vDDDLists object. Actually, this feels correct for me, as it also
+        # allows to define different timezones per exdate line - but client
+        # code has to handle this as list and not blindly expecting to be able
+        # to call event['EXDATE'].to_ical() on it:
+        self.assertEqual(isinstance(exdate, list), True)  # multiple EXDATE
+        self.assertEqual(exdate[0].to_ical(), '20120529T100000')
+
+        # TODO: test for embedded timezone information!
