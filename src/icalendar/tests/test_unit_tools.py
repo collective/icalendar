f45f6b3a6ed83beb9d2daad50ffed3d43550be21
Christian Geier <geier@lostpackets.de>
2013-10-18 19:30:38 +0200

    use compatible strings in string containment tests

--- src/icalendar/tests/test_unit_tools.py
+++ src/icalendar/tests/test_unit_tools.py
@@ -9,19 +9,20 @@ class TestTools(unittest.TestCase):
         # Automatic semi-random uid
         g = UIDGenerator()
         uid = g.uid()
+
         txt = uid.to_ical()
         length = 15 + 1 + 16 + 1 + 11
         self.assertTrue(len(txt) == length)
-        self.assertTrue('@example.com' in txt)
+        self.assertTrue(b'@example.com' in txt)
 
         # You should at least insert your own hostname to be more compliant
         uid = g.uid('Example.ORG')
         txt = uid.to_ical()
         self.assertTrue(len(txt) == length)
-        self.assertTrue('@Example.ORG' in txt)
+        self.assertTrue(b'@Example.ORG' in txt)
 
         # You can also insert a path or similar
         uid = g.uid('Example.ORG', '/path/to/content')
         txt = uid.to_ical()
         self.assertTrue(len(txt) == length)
-        self.assertTrue('-/path/to/content@Example.ORG' in txt)
+        self.assertTrue(b'-/path/to/content@Example.ORG' in txt)
