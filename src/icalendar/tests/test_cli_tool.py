dda867e04908e467e432bd6056da719799c05b6d
jaca <vitouejj@gmail.com>
2022-08-20 12:16:33 +0200

    add test for the cli tool

---
+++ src/icalendar/tests/test_cli_tool.py
@@ -0,0 +1,72 @@
+import unittest
+
+from icalendar import Calendar, cli
+
+INPUT = '''
+BEGIN:VCALENDAR
+VERSION:2.0
+CALSCALE:GREGORIAN
+BEGIN:VEVENT
+SUMMARY:Test Summary
+ORGANIZER:organizer@test.test
+ATTENDEE:attendee1@example.com
+ATTENDEE:attendee2@test.test
+COMMENT:Comment
+DTSTART;TZID=Europe/Warsaw:20220820T103400
+DTEND;TZID=Europe/Warsaw:20220820T113400
+LOCATION:New Amsterdam, 1000 Sunrise Test Street
+DESCRIPTION: Test Description
+END:VEVENT
+BEGIN:VEVENT
+ORGANIZER:organizer@test.test
+ATTENDEE:attendee1@example.com
+ATTENDEE:attendee2@test.test
+SUMMARY:Test summury
+DTSTART;TZID=Europe/Warsaw:20220820T200000
+DTEND;TZID=Europe/Warsaw:20220820T203000
+LOCATION:New Amsterdam, 1010 Test Street
+DESCRIPTION:Test Description\\nThis one is multiline
+END:VEVENT
+END:VCALENDAR
+'''
+
+PROPER_OUTPUT = '''    Organizer: organizer <organizer@test.test>
+    Attendees:
+     attendee1 <attendee1@example.com>
+     attendee2 <attendee2@test.test>
+    Summary    : Test Summary
+    Starts     : Sat Aug 20 10:34:00 2022
+    End        : Sat Aug 20 11:34:00 2022
+    Duration   : 1:00:00
+    Location   : New Amsterdam, 1000 Sunrise Test Street
+    Comment    : Comment
+    Description:
+      Test Description
+
+    Organizer: organizer <organizer@test.test>
+    Attendees:
+     attendee1 <attendee1@example.com>
+     attendee2 <attendee2@test.test>
+    Summary    : Test summury
+    Starts     : Sat Aug 20 20:00:00 2022
+    End        : Sat Aug 20 20:30:00 2022
+    Duration   : 0:30:00
+    Location   : New Amsterdam, 1010 Test Street
+    Comment    : 
+    Description:
+     Test Description
+     This one is multiline
+
+'''
+
+class CLITool(unittest.TestCase):
+    def test_output_is_proper(self):
+        calendar = Calendar.from_ical(INPUT)
+        output = ''
+        for event in calendar.walk('vevent'):
+            output += cli.view(event) + '\n\n'
+        self.assertEqual(PROPER_OUTPUT, output)
+
+if __name__ == '__main__':
+    unittest.main()
+
