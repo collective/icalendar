eb34bc76649f6a3d03238e41f52f88c5c840d324
jaca <vitouejj@gmail.com>
2022-09-09 15:26:47 +0200

    refactor test_issue_345

--- src/icalendar/tests/test_unit_tools.py
+++ src/icalendar/tests/test_unit_tools.py
@@ -1,6 +1,7 @@
+import pytest
 import unittest
-from icalendar.tools import UIDGenerator
 
+from icalendar.tools import UIDGenerator
 
 class TestTools(unittest.TestCase):
 
@@ -26,3 +27,29 @@ class TestTools(unittest.TestCase):
         txt = uid.to_ical()
         self.assertTrue(len(txt) == length)
         self.assertTrue(b'-/path/to/content@Example.ORG' in txt)
+
+@pytest.mark.parametrize('host_name, unique', [
+    ('example.com', ''),
+    ('test.test', ''),
+    ('example.com', '123'),
+    ('test.test', '123')
+])
+def test_uid_generator_uses_host_name(host_name, unique):
+    '''Issue #345 - Why is tools.UIDGenerator a class (that must be instantiated) instead of a module?
+
+    see https://github.com/collective/icalendar/issues/345
+    '''
+    uid = UIDGenerator.uid(host_name=host_name, unique=unique)
+    assert uid.split('@')[1] == host_name
+
+@pytest.mark.parametrize('host_name, unique', [
+    ('example.com', '123'),
+    ('test.test', '123')
+])
+def test_uid_generator_uses_unique(host_name, unique):
+    '''Issue #345 - Why is tools.UIDGenerator a class (that must be instantiated) instead of a module?
+
+    see https://github.com/collective/icalendar/issues/345
+    '''
+    uid = UIDGenerator.uid(host_name=host_name, unique=unique)
+    assert uid.split('-')[1] == f'{unique}@{host_name}'
