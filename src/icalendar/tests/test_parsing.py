60d5781379d28855f98d020baf2d4e65c68167f5
jaca <vitouejj@gmail.com>
2022-09-09 15:30:47 +0200

    refactor test_issue_142 and test_issue_143

--- src/icalendar/tests/test_parsing.py
+++ src/icalendar/tests/test_parsing.py
@@ -1,3 +1,4 @@
+'''Tests checking that parsing works'''
 import pytz
 import pytest
 import base64
@@ -9,6 +10,7 @@ except ModuleNotFoundError:
     from backports import zoneinfo
 
 from icalendar import Event, vBinary, Calendar
+from icalendar.parser import Contentline, Parameters
 
 def test_description_parsed_properly_issue_53(events):
     '''Issue #53 - Parsing failure on some descriptions?
@@ -99,4 +101,15 @@ def test_raises_value_error_for_properties_without_parent_pull_179():
         '''
         with pytest.raises(ValueError):
             Calendar.from_ical('VERSION:2.0')
+
+@pytest.mark.parametrize('raw_content_line, expected_output', [
+    # Issue #142 - Multivalued parameters. This is needed for VCard 3.0.
+    # see https://github.com/collective/icalendar/pull/142
+    ('TEL;TYPE=HOME,VOICE:000000000', ('TEL', Parameters({'TYPE': ['HOME', 'VOICE']}), '000000000')),
+    # Issue #143 - Allow dots in property names. Another vCard related issue.
+    # see https://github.com/collective/icalendar/pull/143
+    ('ITEMADRNULLTHISISTHEADRESS08158SOMECITY12345.ADR:;;This is the Adress 08; Some City;;12345;Germany', ('ITEMADRNULLTHISISTHEADRESS08158SOMECITY12345.ADR', Parameters(), ';;This is the Adress 08; Some City;;12345;Germany'))
+])
+def test_content_lines_parsed_properly(raw_content_line, expected_output):
+    assert Contentline.from_ical(raw_content_line).parts() == expected_output
 
