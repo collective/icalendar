cdff06ea85b43d905ceb018d0476627dbc0fb6e3
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-09-29 00:20:12 +0100

    remove parametrization
    This adds suggested changes
    see https://github.com/collective/icalendar/pull/410#discussion_r982926625

--- src/icalendar/tests/test_issue_104_mark_events_broken.py
+++ src/icalendar/tests/test_issue_104_mark_events_broken.py
@@ -12,12 +12,9 @@ def test_ignore_exceptions_on_broken_events_issue_104(events):
     assert events.issue_104_mark_events_broken.is_broken # TODO: REMOVE FOR NEXT MAJOR RELEASE
     assert events.issue_104_mark_events_broken.errors == [(None, "Content line could not be parsed into parts: 'X': Invalid content line")]
 
-@pytest.mark.parametrize('calendar_file',
-        (('issue_104_broken_calendar.ics'),),
-        indirect=True)
-def test_dont_ignore_exceptions_on_broken_calendars_issue_104(calendar_file):
+def test_dont_ignore_exceptions_on_broken_calendars_issue_104(calendars):
     '''Issue #104 - line parsing error in a VCALENDAR
     (which doesn't have ignore_exceptions). Should raise an exception.
     '''
-    with pytest.raises(ValueError), open(calendar_file) as f:
-        Calendar.from_ical(f.read())
+    with pytest.raises(ValueError):
+        calendars.issue_104_broken_calendar
