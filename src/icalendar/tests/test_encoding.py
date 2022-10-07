ce2ec96451633b8bbd0fa4b6494653ebe55459f6
jaca <vitouejj@gmail.com>
2022-10-07 15:52:51 +0200

    refactor test_encoding_create_from_ical

--- src/icalendar/tests/test_encoding.py
+++ src/icalendar/tests/test_encoding.py
@@ -7,30 +7,24 @@ import icalendar
 import os
 import pytz
 
+@pytest.mark.parametrize('field, expected_value', [
+    ('PRODID', '-//Plönë.org//NONSGML plone.app.event//EN'),
+    ('X-WR-CALDESC', 'test non ascii: äöü ÄÖÜ €'),
+])
+def test_calendar_from_ical_respects_unicode(field, expected_value, calendars):
+    cal = calendars.calendar_with_unicode
+    assert cal[field].to_ical().decode('utf-8') == expected_value
+
+@pytest.mark.parametrize('field, expected_value', [
+    ('SUMMARY', 'Non-ASCII Test: ÄÖÜ äöü €'),
+    ('DESCRIPTION', 'icalendar should be able to handle non-ascii: €äüöÄÜÖ.'),
+    ('LOCATION', 'Tribstrül'),
+])
+def test_event_from_ical_respects_unicode(field, expected_value, events):
+    event = events.event_with_unicode_fields
+    event[field].to_ical().decode('utf-8') == expected_value
 
 class TestEncoding(unittest.TestCase):
-
-    def test_create_from_ical(self):
-        directory = os.path.dirname(__file__)
-        with open(os.path.join(directory, 'encoding.ics'), 'rb') as fp:
-            data = fp.read()
-        cal = icalendar.Calendar.from_ical(data)
-
-        self.assertEqual(cal['prodid'].to_ical().decode('utf-8'),
-                         "-//Plönë.org//NONSGML plone.app.event//EN")
-        self.assertEqual(cal['X-WR-CALDESC'].to_ical().decode('utf-8'),
-                         "test non ascii: äöü ÄÖÜ €")
-
-        event = cal.walk('VEVENT')[0]
-        self.assertEqual(event['SUMMARY'].to_ical().decode('utf-8'),
-                         'Non-ASCII Test: ÄÖÜ äöü €')
-        self.assertEqual(
-            event['DESCRIPTION'].to_ical().decode('utf-8'),
-            'icalendar should be able to handle non-ascii: €äüöÄÜÖ.'
-        )
-        self.assertEqual(event['LOCATION'].to_ical().decode('utf-8'),
-                         'Tribstrül')
-
     def test_create_to_ical(self):
         cal = icalendar.Calendar()
 
