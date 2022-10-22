a237aee19f0122261a08a4d97810e62b47eb2a37
jaca <vitouejj@gmail.com>
2022-10-22 14:42:15 +0200

    merge unique tzid tests into one

--- src/icalendar/tests/test_parsing.py
+++ src/icalendar/tests/test_parsing.py
@@ -161,21 +161,16 @@ def test_creates_event_with_base64_encoded_attachment_issue_82(events):
     event.add('ATTACH', b)
     assert event.to_ical() == events.issue_82_expected_output.raw_ics
 
-def test_converts_unqiue_tzid_if_no_timezone(events, in_timezone):
-    ''' Issue #466 - [BUG] TZID timezone is ignored when forward-slash is used
-    https://github.com/collective/icalendar/issues/466
-    '''
-    start_dt = events.issue_466_convert_tzid_with_slash['dtstart'].dt
-    end_dt = events.issue_466_convert_tzid_with_slash['dtend'].dt
-    assert start_dt == in_timezone(datetime(2022, 10, 21, 20, 0, 0), 'Europe/Stockholm')
-    assert end_dt == in_timezone(datetime(2022, 10, 21, 21, 0, 0), 'Europe/Stockholm')
-
-def test_respects_unique_tzid(calendars, in_timezone):
-    ''' Issue #466 - [BUG] TZID timezone is ignored when forward-slash is used
-    https://github.com/collective/icalendar/issues/466
-    '''
-    start_dt = calendars.issue_466_respect_unique_timezone.walk('VEVENT')[0]['dtstart'].dt
-    end_dt = calendars.issue_466_respect_unique_timezone.walk('VEVENT')[0]['dtend'].dt
+@pytest.mark.parametrize('calendar_name', [
+    # Issue #466 - [BUG] TZID timezone is ignored when forward-slash is used
+    # https://github.com/collective/icalendar/issues/466
+    'issue_466_respect_unique_timezone',
+    'issue_466_convert_tzid_with_slash'
+])
+def test_handles_unique_tzid(calendars, in_timezone, calendar_name):
+    calendar = calendars[calendar_name]
+    start_dt = calendar.walk('VEVENT')[0]['dtstart'].dt
+    end_dt = calendar.walk('VEVENT')[0]['dtend'].dt
     assert start_dt == in_timezone(datetime(2022, 10, 21, 20, 0, 0), 'Europe/Stockholm')
     assert end_dt == in_timezone(datetime(2022, 10, 21, 21, 0, 0), 'Europe/Stockholm')
 
