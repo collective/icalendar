ee84df5f56594cdac80c5532b2f778fe903d7d0e
jaca <vitouejj@gmail.com>
2022-10-23 12:50:16 +0200

    add test from issue #464

--- src/icalendar/tests/test_components_break_on_bad_ics.py
+++ src/icalendar/tests/test_components_break_on_bad_ics.py
@@ -18,3 +18,11 @@ def test_dont_ignore_exceptions_on_broken_calendars_issue_104(calendars):
     '''
     with pytest.raises(ValueError):
         calendars.issue_104_broken_calendar
+
+def test_rdate_dosent_become_none_on_invalid_input_issue_464(events):
+    '''Issue #464 - [BUG] RDATE can become None if value is invalid
+    https://github.com/collective/icalendar/issues/464
+    '''
+    assert events.issue_464_invalid_rdate.is_broken
+    assert ('RDATE', 'Expected period format, got: 199709T180000Z/PT5H30M') in events.issue_464_invalid_rdate.errors
+    assert not b'RDATE:None' in events.issue_464_invalid_rdate.to_ical()
