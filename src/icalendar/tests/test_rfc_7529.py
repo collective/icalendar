003602daed1c888d8169532964ecb8868811d97c
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-06-28 15:55:00 +0100

    Add vMonth to represent the leap month parameter for RFC 7529

--- src/icalendar/tests/test_rfc_7529.py
+++ src/icalendar/tests/test_rfc_7529.py
@@ -5,6 +5,7 @@ See
 - https://www.rfc-editor.org/rfc/rfc7529.html
 """
 import pytest
+from icalendar.prop import vRecur, vMonth
 
 
 @pytest.mark.parametrize(
@@ -23,3 +24,41 @@ def test_rscale(calendars, uid, scale):
     rrule = event["RRULE"]
     print(rrule)
     assert rrule["RSCALE"] == [scale]
+
+
+@pytest.mark.parametrize(
+    "uid,skip",
+    [
+        ("4.3.2", None),
+        ("4.3.3", ["FORWARD"]),
+    ]
+)
+def test_rscale(calendars, uid, skip):
+    """Check that the RSCALE is parsed correctly."""
+    event = calendars.rfc_7529.walk(select=lambda c: c.get("UID") == uid)[0]
+    recur = event["RRULE"]
+    assert recur.get("SKIP") == skip
+
+
+def test_leap_month(calendars):
+    """Check that we can parse the leap month."""
+    event = calendars.rfc_7529.walk(select=lambda c: c.get("UID") == "4.3.3")[0]
+    recur = event["RRULE"]
+    assert recur["BYMONTH"][0].leap is True
+
+
+@pytest.mark.parametrize(
+    "ty, recur, ics",
+    [
+        (vRecur, vRecur(rscale="CHINESE", freq="YEARLY"), b"RSCALE=CHINESE;FREQ=YEARLY"),
+        (vRecur, vRecur(bymonth=vMonth(10)), b"BYMONTH=10"),
+        (vRecur, vRecur(bymonth=vMonth("5L")), b"BYMONTH=5L"),
+        (vMonth, vMonth(10), b"10"),
+        (vMonth, vMonth("5L"), b"5L"),
+    ]
+)
+def test_conversion(ty, recur, ics):
+    """Test string conversion."""
+    assert recur.to_ical() == ics
+    assert ty.from_ical(ics.decode()) == recur
+    assert ty.from_ical(ics.decode()).to_ical() == ics
