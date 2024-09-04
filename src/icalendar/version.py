2b8a9f76b8a4d5fc8102a2d5c3651f716b3d3d54
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-09-04 15:51:34 +0100

    build works
    python -m build

---
+++ src/icalendar/version.py
@@ -0,0 +1,13 @@
+"""Version file as a stable interface for the generated _version.py file."""
+try:
+    from ._version import __version__, __version_tuple__, version, version_tuple
+except ModuleNotFoundError:
+    __version__ = version = "0.0.0dev0"
+    __version_tuple__ = version_tuple = (0, 0, 0, "dev0")
+
+__all__ = [
+    "__version__",
+    "version",
+    "__version_tuple__",
+    "version_tuple",
+]
