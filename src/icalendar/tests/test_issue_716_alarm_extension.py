6db6147a8d4a113eb1e4547a342ee43229774c8e
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-10-16 12:43:20 +0100

    Update src/icalendar/tests/test_issue_716_alarm_extension.py
    Co-authored-by: Steve Piercy <web@stevepiercy.com>

--- src/icalendar/tests/test_issue_716_alarm_extension.py
+++ src/icalendar/tests/test_issue_716_alarm_extension.py
@@ -4,7 +4,7 @@ Events can have alarms.
 Alarms can be in this state:
 
 - active - the user wants the alarm to pop up
-- acknowledged - the use does not want that alarm to go off any more
+- acknowledged - the user no longer wants the alarm
 - snoozed - the user moved that alarm to another time
 
 The alarms can only work on the properties of the event like
