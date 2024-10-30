b3fa5b65330cd7e54fda023b4ddee2128a747704
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-10-30 08:09:44 +0000

    remove old file

--- src/icalendar/tests/test_issue_716_alarm_time_computation.py
+++ src/icalendar/tests/test_issue_716_alarm_time_computation.py
@@ -1,4 +1,17 @@
-"""Test the alarm time computation."""
+"""Test the alarm time computation.
+
+Events can have alarms.
+Alarms can be in this state:
+
+- active - the user wants the alarm to pop up
+- acknowledged - the user no longer wants the alarm
+- snoozed - the user moved that alarm to another time
+
+The alarms can only work on the properties of the event like
+DTSTART, DTEND and DURATION.
+
+"""
+
 
 from datetime import date, datetime, timedelta, timezone
 
