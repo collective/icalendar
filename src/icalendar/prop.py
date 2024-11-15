9f82ec87356e885b4b47d64d877f4ae65c3e9012
Şerif <serifserif135606534@gmail.com>
2024-11-15 16:31:41 +0300

    issue-742

--- src/icalendar/prop.py
+++ src/icalendar/prop.py
@@ -350,7 +350,42 @@ class vDDDTypes(TimeBase):
 
 
 class vDate(TimeBase):
-    """Render and generates iCalendar date format.
+    """Date
+    Value Name:  DATE
+    
+    Purpose:  This value type is used to identify values that contain a
+      calendar date.
+      
+    Format Definition:  This value type is defined by the following
+      notation:
+
+       date               = date-value
+
+       date-value         = date-fullyear date-month date-mday
+       date-fullyear      = 4DIGIT
+       date-month         = 2DIGIT        ;01-12
+       date-mday          = 2DIGIT        ;01-28, 01-29, 01-30, 01-31
+                                          ;based on month/year
+                                          
+    Description:  If the property permits, multiple "date" values are
+      specified as a COMMA-separated list of values.  The format for the
+      value type is based on the [ISO.8601.2004] complete
+      representation, basic format for a calendar date.  The textual
+      format specifies a four-digit year, two-digit month, and two-digit
+      day of the month.  There are no separator characters between the
+      year, month, and day component text.
+    
+    Example:  The following represents July 14, 1997:
+
+       19970714
+       
+       >>> date = vDate.from_ical('19970714')
+       >>> date.year
+       1997
+       >>> date.month
+       7
+       >>> date.day
+       14
     """
 
     def __init__(self, dt):
