e86e6ff9ed9b276742336ae030ef78885d3eba50
Natasha Mattson <22650546+natashamm@users.noreply.github.com>
2024-11-22 17:31:08 -0800

    test Period overlaps

--- src/icalendar/tests/test_period.py
+++ src/icalendar/tests/test_period.py
@@ -9,7 +9,7 @@ import datetime
 
 import pytest
 
-from icalendar.prop import vDDDTypes
+from icalendar.prop import vDDDTypes, vPeriod
 
 
 @pytest.mark.parametrize(
@@ -95,3 +95,18 @@ def test_tzid_is_part_of_the_period_values(calendars, tzp):
         datetime.datetime(2023, 12, 13, 12), "America/Vancouver"
     )
     assert end == tzp.localize(datetime.datetime(2023, 12, 13, 15), "America/Vancouver")
+
+
+def test_period_overlaps():
+    # 30 minute increments
+    datetime_1 = datetime.datetime(2024, 11, 20, 12, 0)  # 12:00
+    datetime_2 = datetime.datetime(2024, 11, 20, 12, 30) # 12:30
+    datetime_3 = datetime.datetime(2024, 11, 20, 13, 0)  # 13:00
+    
+    period_1 = vPeriod((datetime_1,datetime_2))
+    period_2 = vPeriod((datetime_1,datetime_3))
+    period_3 = vPeriod((datetime_2,datetime_3))
+
+    assert period_1.overlaps(period_2)
+    assert period_3.overlaps(period_2)
+    assert not period_1.overlaps(period_3)
