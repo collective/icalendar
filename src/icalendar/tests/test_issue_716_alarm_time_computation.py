8f1c77376fa8c65eac1c53ecf29c372062b96fd9
Nicco Kunzmann <niccokunzmann@rambler.ru>
2025-03-19 11:48:32 +0000

    format test code with ruff

--- src/icalendar/tests/test_issue_716_alarm_time_computation.py
+++ src/icalendar/tests/test_issue_716_alarm_time_computation.py
@@ -12,7 +12,6 @@ DTSTART, DTEND, and DURATION.
 
 """
 
-
 from datetime import date, datetime, timedelta, timezone
 
 import pytest
@@ -46,12 +45,8 @@ alarm_1.add("TRIGGER", EXAMPLE_TRIGGER)
 alarm_2 = Alarm()
 alarm_2["TRIGGER"] = vDatetime(EXAMPLE_TRIGGER)
 
-@pytest.mark.parametrize(
-    "alarm",
-    [
-        alarm_1, alarm_2
-    ]
-)
+
+@pytest.mark.parametrize("alarm", [alarm_1, alarm_2])
 def test_absolute_alarm_time_with_vDatetime(alarm):
     """Check that the absolute alarm is recognized.
 
@@ -71,6 +66,7 @@ alarm_incomplete_1.DURATION = timedelta(hours=1)
 alarm_incomplete_2 = Alarm()
 alarm_incomplete_2.TRIGGER = timedelta(hours=2)
 alarm_incomplete_2.REPEAT = 100
+
 
 @pytest.mark.parametrize("alarm", [alarm_incomplete_1, alarm_incomplete_2])
 def test_alarm_has_only_one_of_repeat_or_duration(alarm):
@@ -95,21 +91,34 @@ def test_cannot_compute_relative_alarm_without_start(alarm_before_start):
     """We have an alarm without a start of a component."""
     with pytest.raises(IncompleteAlarmInformation) as e:
         Alarms(alarm_before_start).times  # noqa: B018
-    assert e.value.args[0] == f"Use {Alarms.__name__}.{Alarms.set_start.__name__} because at least one alarm is relative to the start of a component."
+    assert (
+        e.value.args[0]
+        == f"Use {Alarms.__name__}.{Alarms.set_start.__name__} because at least one alarm is relative to the start of a component."
+    )
 
 
 @pytest.mark.parametrize(
     ("dtstart", "timezone", "trigger"),
     [
-        (datetime(2024, 10, 29, 13, 10), "UTC", datetime(2024, 10, 29, 13, 10, tzinfo=UTC)),
+        (
+            datetime(2024, 10, 29, 13, 10),
+            "UTC",
+            datetime(2024, 10, 29, 13, 10, tzinfo=UTC),
+        ),
         (date(2024, 11, 16), None, datetime(2024, 11, 16, 0, 0)),
-        (datetime(2024, 10, 29, 13, 10), "Asia/Singapore", datetime(2024, 10, 29, 5, 10, tzinfo=UTC)),
+        (
+            datetime(2024, 10, 29, 13, 10),
+            "Asia/Singapore",
+            datetime(2024, 10, 29, 5, 10, tzinfo=UTC),
+        ),
         (datetime(2024, 10, 29, 13, 20), None, datetime(2024, 10, 29, 13, 20)),
-    ]
+    ],
 )
-def test_can_complete_relative_calculation_if_a_start_is_given(alarm_before_start, dtstart, timezone, trigger, tzp):
+def test_can_complete_relative_calculation_if_a_start_is_given(
+    alarm_before_start, dtstart, timezone, trigger, tzp
+):
     """The start is given and required."""
-    start = (dtstart if timezone is None else tzp.localize(dtstart, timezone))
+    start = dtstart if timezone is None else tzp.localize(dtstart, timezone)
     alarms = Alarms(alarm_before_start)
     alarms.set_start(start)
     assert len(alarms.times) == 1
@@ -131,21 +140,32 @@ def test_cannot_compute_relative_alarm_without_end(alarms):
     """We have an alarm without an end of a component."""
     with pytest.raises(IncompleteAlarmInformation) as e:
         Alarms(alarms.rfc_5545_end).times  # noqa: B018
-    assert e.value.args[0] == f"Use {Alarms.__name__}.{Alarms.set_end.__name__} because at least one alarm is relative to the end of a component."
+    assert (
+        e.value.args[0]
+        == f"Use {Alarms.__name__}.{Alarms.set_end.__name__} because at least one alarm is relative to the end of a component."
+    )
 
 
 @pytest.mark.parametrize(
     ("dtend", "timezone", "trigger"),
     [
-        (datetime(2024, 10, 29, 13, 10), "UTC", datetime(2024, 10, 29, 13, 10, tzinfo=UTC)),
+        (
+            datetime(2024, 10, 29, 13, 10),
+            "UTC",
+            datetime(2024, 10, 29, 13, 10, tzinfo=UTC),
+        ),
         (date(2024, 11, 16), None, date(2024, 11, 16)),
-        (datetime(2024, 10, 29, 13, 10), "Asia/Singapore", datetime(2024, 10, 29, 5, 10, tzinfo=UTC)),
+        (
+            datetime(2024, 10, 29, 13, 10),
+            "Asia/Singapore",
+            datetime(2024, 10, 29, 5, 10, tzinfo=UTC),
+        ),
         (datetime(2024, 10, 29, 13, 20), None, datetime(2024, 10, 29, 13, 20)),
-    ]
+    ],
 )
 def test_can_complete_relative_calculation(alarms, dtend, timezone, trigger, tzp):
     """The start is given and required."""
-    start = (dtend if timezone is None else tzp.localize(dtend, timezone))
+    start = dtend if timezone is None else tzp.localize(dtend, timezone)
     alarms = Alarms(alarms.rfc_5545_end)
     alarms.set_end(start)
     assert len(alarms.times) == 1
@@ -161,7 +181,6 @@ def test_end_as_date_with_delta_as_date_stays_date(alarms, dtend):
     a.set_end(dtend)
     assert len(a.times) == 1
     assert a.times[0].trigger == dtend - timedelta(days=2)
-
 
 
 def test_add_multiple_alarms(alarms):
@@ -202,31 +221,82 @@ def test_cannot_set_the_event_twice(calendars):
     [
         ("alarm_etar_future", -1, 3, "Etar (1): we just created the alarm"),
         ("alarm_etar_notification", -1, 2, "Etar (2): the notification popped up"),
-        ("alarm_etar_notification_clicked", -1, 0, "Etar (3): the notification was dismissed"),  # TODO: check that that is really true
-        ("alarm_google_future", -1, 4, "Google (1): we just created the event with alarms"),
-        ("alarm_google_acknowledged", -1, 2, "Google (2): 2 alarms happened at the same time"),
+        (
+            "alarm_etar_notification_clicked",
+            -1,
+            0,
+            "Etar (3): the notification was dismissed",
+        ),  # TODO: check that that is really true
+        (
+            "alarm_google_future",
+            -1,
+            4,
+            "Google (1): we just created the event with alarms",
+        ),
+        (
+            "alarm_google_acknowledged",
+            -1,
+            2,
+            "Google (2): 2 alarms happened at the same time",
+        ),
         ("alarm_thunderbird_future", -1, 2, "Thunderbird (1.1): 2 alarms are set"),
-        ("alarm_thunderbird_snoozed_until_1457", -1, 2, "Thunderbird (1.2): 2 alarms are snoozed to another time"),
-        ("alarm_thunderbird_closed", -1, 0, "Thunderbird (1.3): all alarms are dismissed (closed)"),
+        (
+            "alarm_thunderbird_snoozed_until_1457",
+            -1,
+            2,
+            "Thunderbird (1.2): 2 alarms are snoozed to another time",
+        ),
+        (
+            "alarm_thunderbird_closed",
+            -1,
+            0,
+            "Thunderbird (1.3): all alarms are dismissed (closed)",
+        ),
         ("alarm_thunderbird_2_future", -1, 2, "Thunderbird (2.1): 2 alarms active"),
-        ("alarm_thunderbird_2_notification_popped_up", -1, 2, "Thunderbird (2.2): one alarm popped up as a notification"),
-        ("alarm_thunderbird_2_notification_5_min_postponed", -1, 2, "Thunderbird (2.3): 1 alarm active and one postponed by 5 minutes"),
-        ("alarm_thunderbird_2_notification_5_min_postponed_and_popped_up", -1, 2, "Thunderbird (2.4): 1 alarm active and one postponed by 5 minutes and now popped up"),
-        ("alarm_thunderbird_2_notification_5_min_postponed_and_closed", -1, 1, "Thunderbird (2.5): 1 alarm active and one postponed by 5 minutes and is now acknowledged"),
-    ]
+        (
+            "alarm_thunderbird_2_notification_popped_up",
+            -1,
+            2,
+            "Thunderbird (2.2): one alarm popped up as a notification",
+        ),
+        (
+            "alarm_thunderbird_2_notification_5_min_postponed",
+            -1,
+            2,
+            "Thunderbird (2.3): 1 alarm active and one postponed by 5 minutes",
+        ),
+        (
+            "alarm_thunderbird_2_notification_5_min_postponed_and_popped_up",
+            -1,
+            2,
+            "Thunderbird (2.4): 1 alarm active and one postponed by 5 minutes and now popped up",
+        ),
+        (
+            "alarm_thunderbird_2_notification_5_min_postponed_and_closed",
+            -1,
+            1,
+            "Thunderbird (2.5): 1 alarm active and one postponed by 5 minutes and is now acknowledged",
+        ),
+    ],
 )
-def test_number_of_active_alarms_from_calendar_software(calendars, calendar, index, count, message):
+def test_number_of_active_alarms_from_calendar_software(
+    calendars, calendar, index, count, message
+):
     """Check that we extract calculate the correct amount of active alarms."""
     event = calendars[calendar].subcomponents[index]
     a = Alarms(event)
-    active_alarms = a.active  # We do not need to pass a timezone because the events have a timezone
-    assert len(active_alarms) == count, f"{message} - I expect {count} alarms active but got {len(active_alarms)}."
+    active_alarms = (
+        a.active
+    )  # We do not need to pass a timezone because the events have a timezone
+    assert (
+        len(active_alarms) == count
+    ), f"{message} - I expect {count} alarms active but got {len(active_alarms)}."
 
 
 three_alarms = Alarm()
 three_alarms.REPEAT = 2
-three_alarms.add("DURATION", timedelta(hours=1)) # 2 hours & 1 hour before
-three_alarms.add("TRIGGER", -timedelta(hours=3)) # 3 hours before
+three_alarms.add("DURATION", timedelta(hours=1))  # 2 hours & 1 hour before
+three_alarms.add("TRIGGER", -timedelta(hours=3))  # 3 hours before
 
 
 @pytest.mark.parametrize(
@@ -236,10 +306,17 @@ three_alarms.add("TRIGGER", -timedelta(hours=3)) # 3 hours before
         (datetime(2024, 10, 10, 12), datetime(2024, 10, 10, 9, 1), "UTC", 2),
         (datetime(2024, 10, 10, 12), datetime(2024, 10, 10, 10, 1), "UTC", 1),
         (datetime(2024, 10, 10, 12), datetime(2024, 10, 10, 11, 1), "UTC", 0),
-        (datetime(2024, 10, 10, 12, tzinfo=timezone.utc), datetime(2024, 10, 10, 11, 1), None, 0),
-    ]
+        (
+            datetime(2024, 10, 10, 12, tzinfo=timezone.utc),
+            datetime(2024, 10, 10, 11, 1),
+            None,
+            0,
+        ),
+    ],
 )
-def test_number_of_active_alarms_with_moving_time(start, acknowledged, count, tzp, timezone):
+def test_number_of_active_alarms_with_moving_time(
+    start, acknowledged, count, tzp, timezone
+):
     """Check how many alarms are active after a time they are acknowledged."""
     a = Alarms()
     a.add_alarm(three_alarms)
@@ -258,7 +335,10 @@ def test_incomplete_alarm_information_for_active_state(tzp):
     a.acknowledge_until(tzp.localize_utc(datetime(2012, 10, 10, 12)))
     with pytest.raises(IncompleteAlarmInformation) as e:
         a.active  # noqa: B018
-    assert e.value.args[0] == f"A local timezone is required to check if the alarm is still active. Use Alarms.{Alarms.set_local_timezone.__name__}()."
+    assert (
+        e.value.args[0]
+        == f"A local timezone is required to check if the alarm is still active. Use Alarms.{Alarms.set_local_timezone.__name__}()."
+    )
 
 
 @pytest.mark.parametrize(
@@ -269,7 +349,7 @@ def test_incomplete_alarm_information_for_active_state(tzp):
         "alarm_thunderbird_closed",
         "alarm_thunderbird_future",
         "alarm_thunderbird_snoozed_until_1457",
-    ]
+    ],
 )
 def test_thunderbird_recognition(calendars, calendar_name):
     """Check if we correctly discover Thunderbird's alarm algorithm."""
@@ -282,12 +362,12 @@ def test_thunderbird_recognition(calendars, calendar_name):
 @pytest.mark.parametrize(
     "snooze",
     [
-        datetime(2012, 10, 10, 11, 1), # before everything
+        datetime(2012, 10, 10, 11, 1),  # before everything
         datetime(2017, 12, 1, 10, 1),
         datetime(2017, 12, 1, 11, 1),
         datetime(2017, 12, 1, 12, 1),
-        datetime(2017, 12, 1, 13, 1), # snooze until after the start of the event
-    ]
+        datetime(2017, 12, 1, 13, 1),  # snooze until after the start of the event
+    ],
 )
 def test_snoozed_alarm_has_trigger_at_snooze_time(tzp, snooze):
     """When an alarm is snoozed, it pops up after the snooze time."""
@@ -322,7 +402,7 @@ def test_snoozed_alarm_has_trigger_at_snooze_time(tzp, snooze):
         # When the second "snooze" alarm is triggered, the user decides to dismiss it.
         # The client acknowledges both the original alarm and the second "snooze" alarm:
         (4, ()),
-    ]
+    ],
 )
 def test_rfc_9074_alarm_times(events, event_index, alarm_times):
     """Test the examples from the RFC and their timing.
@@ -331,7 +411,9 @@ def test_rfc_9074_alarm_times(events, event_index, alarm_times):
     """
     a = events[f"rfc_9074_example_{event_index}"].alarms
     assert len(a.active) == len(alarm_times)
-    expected_alarm_times = {vDatetime.from_ical(t, "America/New_York") for t in alarm_times}
+    expected_alarm_times = {
+        vDatetime.from_ical(t, "America/New_York") for t in alarm_times
+    }
     computed_alarm_times = {alarm.trigger for alarm in a.active}
     assert expected_alarm_times == computed_alarm_times
 
