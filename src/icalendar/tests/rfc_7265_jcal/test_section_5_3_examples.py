551ecc155acc46087866e5faee055850abd31c90
Nicco Kunzmann <niccokunzmann@rambler.ru>
2025-11-14 17:59:09 +0000

    add more examples

--- src/icalendar/tests/rfc_7265_jcal/test_section_5_3_examples.py
+++ src/icalendar/tests/rfc_7265_jcal/test_section_5_3_examples.py
@@ -10,7 +10,7 @@ from icalendar.parser_tools import to_unicode
 
 def test_convert_coffee(calendars):
     """convert the unknown value property"""
-    calendar = calendars.rfc_7265_unknown_parameter
+    calendar = calendars.rfc_7265_example_2
     ical = calendar.to_ical().decode()
     print(to_unicode(ical))
     assert r"X-COFFEE-DATA:Stenophylla\;Guinea\\\,Africa" in ical
@@ -42,6 +42,20 @@ def test_know_attributes_do_not_receive_a_VALUE(prop_name, value, expected_value
 
 def test_conversion_from_ical(calendars):
     """Convert the example to jcal."""
-    calendar: Calendar = calendars.rfc_7265_property_unknown
+    calendar: Calendar = calendars.rfc_7265_example_1
     jcal = calendar.to_jcal()
     assert jcal[1][0] == ["x-complaint-deadline", {}, "unknown", "20110512T120000Z"]
+
+
+def test_example_3(calendars):
+    """Test the 3rd example."""
+    calendar = calendars.rfc_7265_example_3
+    ical = calendar.to_ical().decode()
+    assert "PERCENT-COMPLETE:95" in ical
+
+
+def test_example_4(events):
+    """Test the 4th example."""
+    event = events.rfc_7265_example_4
+    ical = event.to_jcal()
+    assert ical[1][0] == ["dtstart", {"x-slack": "30.3"}, "date", "2011-05-12"]
