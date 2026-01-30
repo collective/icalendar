3a395f5e0c3ac8067b2c43199ba3e7cbd14f5a10
Likhith Ponnada <plnlikhith@gmail.com>
2026-01-30 22:41:11 +0530

    Update src/icalendar/cal/component.py
    Co-authored-by: Nicco Kunzmann <niccokunzmann@rambler.ru>

--- src/icalendar/cal/component.py
+++ src/icalendar/cal/component.py
@@ -405,7 +405,7 @@ class Component(CaselessDict):
             name = name.upper()
         return self._walk(name, select)
 
-    def with_uid(self, uid):
+    def with_uid(self, uid:str) -> Component:
         """Return a list of components with the given UID."""
         return [c for c in self.walk() if c.get('uid') == uid]
 
