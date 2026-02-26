595203d202c6ab7378c82a99b8f0c46ed43097f8
Nicco Kunzmann <niccokunzmann@rambler.ru>
2026-02-26 19:11:30 +0000

    Do not run tests if review requests changes
    Also test all the conditions

---
+++ .github/workflows/test_generate_matrix.py
@@ -0,0 +1,46 @@
+"""Test that we get the right output under all conditions.
+
+Run with:
+
+    pytest .github/workflows/test_generate_matrix.py
+
+"""
+
+import pytest
+from generate_matrix import generate_matrix
+
+COUNT_ALL = 7
+COUNT_MIN = 3
+
+
+@pytest.mark.parametrize(
+    ("arg_pr", "expected", "arg_ref"),
+    [
+        # push
+        ("", COUNT_ALL, "refs/heads/main"),
+        ("", COUNT_ALL, "refs/heads/7.x"),
+        ("", COUNT_ALL, "refs/heads/6.x"),
+        ("", COUNT_ALL, "refs/heads/5.x"),
+        ("", COUNT_ALL, "refs/tags/v7.0.2"),
+        ("", COUNT_MIN, "refs/heads/pr-branch"),
+        # review
+        ("changes_requested", 0, "refs/heads/main"),
+        ("changes_requested", 0, "refs/heads/7.x"),
+        ("changes_requested", 0, "refs/heads/6.x"),
+        ("changes_requested", 0, "refs/heads/5.x"),
+        ("changes_requested", 0, "refs/tags/v7.0.2"),
+        ("changes_requested", 0, "refs/heads/pr-branch"),
+        ("approved", COUNT_ALL, "refs/heads/main"),
+        ("approved", COUNT_ALL, "refs/heads/7.x"),
+        ("approved", COUNT_ALL, "refs/heads/6.x"),
+        ("approved", COUNT_ALL, "refs/heads/5.x"),
+        ("approved", COUNT_ALL, "refs/tags/v7.0.2"),
+        ("approved", COUNT_ALL, "refs/heads/pr-branch"),
+    ],
+)
+def test_count_test_runs(arg_ref, arg_pr, expected):
+    """Check which values we get."""
+    matrix = generate_matrix(arg_ref, arg_pr)
+    for case in matrix:
+        print(f"- {case['test_name']}")
+    assert len(matrix) == expected, f"Expected {expected} test runs, got {len(matrix)}"
