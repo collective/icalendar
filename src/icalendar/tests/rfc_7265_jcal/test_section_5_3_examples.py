73c4e9517f44923a948c3a96c61beb5befe41dbf
Filip Atanasovski <55002223+FilipAtanasovski-dev@users.noreply.github.com>
2026-03-19 18:52:19 +0100

    ReAdding test_section_5_3_examples.py

--- src/icalendar/tests/rfc_7265_jcal/test_section_5_3_examples.py
+++ src/icalendar/tests/rfc_7265_jcal/test_section_5_3_examples.py
@@ -1,11 +1,11 @@
 """Check that we convert the examples propertly."""
 
-from typing import TYPE_CHECKING
+from pprint import pprint
 
+import pytest
+
+from icalendar import Calendar, Event, vText
 from icalendar.parser_tools import to_unicode
-
-if TYPE_CHECKING:
-    from icalendar import Calendar
 
 
 def test_convert_coffee(calendars):
@@ -14,6 +14,29 @@ def test_convert_coffee(calendars):
     ical = calendar.to_ical().decode()
     print(to_unicode(ical))
     assert r"X-COFFEE-DATA:Stenophylla\;Guinea\\\,Africa" in ical
+
+
+@pytest.mark.parametrize(
+    ("prop_name", "value", "expected_value"),
+    [
+        ("SUMMARY", "TEXT", "TEXT"),
+        ("SUMMARY", "URI", "URI"),
+        ("DESCRIPTION", "URI", "URI"),
+        ("X-ALT-DESC", "UNKNOWN", "UNKNOWN"),
+        ("DESCRIPTION", "TEXT", "TEXT"),
+    ],
+)
+def test_know_attributes_do_not_receive_a_VALUE(prop_name, value, expected_value):
+    """If we do not know a certain attribute, we always use the unknown property type."""
+    event = Event()
+    prop = vText("XXX")
+    prop.VALUE = value
+    event[prop_name] = prop
+    jcal = event.to_jcal()
+    pprint(jcal)
+    event2 = Event.from_jcal(jcal)
+    pprint(event2.to_jcal())
+    assert event2[prop_name].VALUE == expected_value
 
 
 def test_conversion_from_ical(calendars):
