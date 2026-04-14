67954eb1712a85ba7629731d5405c79f7c691b82
Aayan Khan <aayan.khan02072007@gmail.com>
2026-04-14 19:37:29 +0530

    Updated with usage and example

--- src/icalendar/param.py
+++ src/icalendar/param.py
@@ -65,28 +65,30 @@ def string_parameter(
         A property object with a getter, setter, and deleter for the parameter.
 
     Example:
-
         Define a parameter that is stored as a string but used as an integer:
-
-        priority = string_parameter(
-            "PRIORITY",
-            "Priority of the component",
-            default=lambda: 0,
-            convert=int,
-            convert_to=str,
-        )
+        
+        .. code-block:: python
+        
+            priority = string_parameter(
+                "PRIORITY",
+                "Priority of the component",
+                default=lambda: 0,
+                convert=int,
+                convert_to=str,
+            )
 
         Accessing the property converts the stored value:
 
-        obj.params["PRIORITY"] = "5"
-        obj.priority
-        5
+        .. code-block:: python
 
+            obj.params["PRIORITY"] = "5"
+            obj.priority # Returns 5 (int)
+            
         Setting the property stores it as a string:
 
-        obj.priority = 10
-        obj.params["PRIORITY"]
-        "10"
+        .. code-block:: python
+            obj.priority = 10
+            obj.params["PRIORITY"] # Returns "10" (str)
     """
 
     if convert_to is None:
