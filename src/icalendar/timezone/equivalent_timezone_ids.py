collective/icalendar

commit 0c4ea18bdd0f37fe46aea43a3bda96590d4ee3c6

ruff format src/icalendar/timezone/

src/icalendar/timezone/equivalent_timezone_ids.py
@@ -21,11 +21,13 @@ from __future__ import annotations
 from collections import defaultdict
 from datetime import datetime, timedelta, tzinfo
 from pathlib import Path
-from pprint import pprint
-from typing import Callable, NamedTuple, Optional
+from typing import TYPE_CHECKING, NamedTuple
+from zoneinfo import ZoneInfo, available_timezones
 
 from pytz import AmbiguousTimeError, NonExistentTimeError
-from zoneinfo import ZoneInfo, available_timezones
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
 
 START = datetime(1970, 1, 1)  # noqa: DTZ001
 END = datetime(2020, 1, 1)  # noqa: DTZ001
@@ -35,9 +37,9 @@ DTS = []
 dt = START
 while dt <= END:
     DTS.append(dt)
-    dt += timedelta(
-        hours=25
-    )  # This must be big enough to be fast and small enough to identify the timeszones before it is the present year
+    dt += timedelta(hours=25)
+    # This must be big enough to be fast and small enough to identify the timeszones
+    # before it is the present year
 del dt
 
 
@@ -50,7 +52,6 @@ def main(
     We cannot create one lookup for all because they seem to be all equivalent
     if we mix timezone implementations.
     """
-    print(create_timezones, name)
     unsorted_tzids = available_timezones()
     unsorted_tzids.remove("localtime")
     unsorted_tzids.remove("Factory")
@@ -70,13 +71,11 @@ def main(
         step: timedelta = timedelta(hours=1),
         start: datetime = START,
         end: datetime = END,
-        todo: Optional[set[str]] = None,
+        todo: set[str] | None = None,
     ) -> tuple[datetime, dict[timedelta, set[str]]] | set[str]:  # should be recursive
         """Generate a lookup tree."""
         if todo is None:
             todo = [tz.id for tz in tzs]
-        print(f"{len(todo)} left to compute")
-        print(len(tzs))
         if len(tzs) == 0:
             raise ValueError("tzs cannot be empty")
         if len(tzs) == 1:
@@ -104,12 +103,11 @@ def main(
                 start += step
                 continue
             lookup = {}
-            for offset, tzs in offsets.items():
+            for offset, tzs2 in offsets.items():
                 lookup[offset] = generate_tree(
-                    tzs=tzs, step=step, start=start + step, end=end, todo=todo
+                    tzs=tzs2, step=step, start=start + step, end=end, todo=todo
                 )
             return start, lookup
-        print(f"reached end with {len(tzs)} timezones - assuming they are equivalent.")
         result = set()
         for tz in tzs:
             result.add(tz.id)
@@ -119,16 +117,12 @@ def main(
     lookup = generate_tree(tzs, step=timedelta(hours=33))
 
     file = Path(__file__).parent / f"equivalent_timezone_ids_{name}.py"
-    print(f"The result is written to {file}.")
-    print("lookup = ", end="")
-    pprint(lookup)
     with file.open("w") as f:
         f.write(
             f"'''This file is automatically generated by {Path(__file__).name}'''\n"
         )
         f.write("import datetime\n\n")
         f.write("\nlookup = ")
-        pprint(lookup, stream=f)
         f.write("\n\n__all__ = ['lookup']\n")
 
     return lookup
@@ -137,9 +131,10 @@ def main(
 __all__ = ["main"]
 
 if __name__ == "__main__":
+    from zoneinfo import ZoneInfo
+
     from dateutil.tz import gettz
     from pytz import timezone
-    from zoneinfo import ZoneInfo
 
     # add more timezone implementations if you like
     main(

src/icalendar/timezone/equivalent_timezone_ids_result.py
@@ -2,7 +2,6 @@
 
 import datetime
 
-
 lookup = (
     datetime.datetime(1970, 1, 2, 0, 0),
     {

src/icalendar/timezone/provider.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from datetime import datetime, tzinfo
@@ -30,19 +30,20 @@ class TZProvider(ABC):
         """Localize a datetime to a timezone."""
 
     @abstractmethod
-    def knows_timezone_id(self, id: str) -> bool:
+    def knows_timezone_id(self, tzid: str) -> bool:
         """Whether the timezone is already cached by the implementation."""
 
     @abstractmethod
     def fix_rrule_until(self, rrule: rrule, ical_rrule: prop.vRecur) -> None:
-        """Make sure the until value works for the rrule generated from the ical_rrule."""
+        """Make sure the until value works for the rrule generated from the ical_rrule.
+        """
 
     @abstractmethod
     def create_timezone(self, name: str, transition_times, transition_info) -> tzinfo:
         """Create a pytz timezone file given information."""
 
     @abstractmethod
-    def timezone(self, name: str) -> Optional[tzinfo]:
+    def timezone(self, name: str) -> tzinfo | None:
         """Return a timezone with a name or None if we cannot find it."""
 
     @abstractmethod

src/icalendar/timezone/pytz.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from datetime import datetime, tzinfo
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 
 import pytz
 from pytz.tzinfo import DstTzInfo
@@ -33,16 +33,17 @@ class PYTZ(TZProvider):
         """Localize a datetime to a timezone."""
         return tz.localize(dt)
 
-    def knows_timezone_id(self, id: str) -> bool:
+    def knows_timezone_id(self, tzid: str) -> bool:
         """Whether the timezone is already cached by the implementation."""
-        return id in pytz.all_timezones
+        return tzid in pytz.all_timezones
 
     def fix_rrule_until(self, rrule: rrule, ical_rrule: prop.vRecur) -> None:
-        """Make sure the until value works for the rrule generated from the ical_rrule."""
+        """Make sure the until value works for the rrule generated from the ical_rrule.
+        """
         if not {"UNTIL", "COUNT"}.intersection(ical_rrule.keys()):
             # pytz.timezones don't know any transition dates after 2038
             # either
-            rrule._until = datetime(2038, 12, 31, tzinfo=pytz.UTC)
+            rrule._until = datetime(2038, 12, 31, tzinfo=pytz.UTC)  # noqa: SLF001
 
     def create_timezone(self, tz: Timezone.Timezone) -> tzinfo:
         """Create a pytz timezone from the given information."""
@@ -59,7 +60,7 @@ class PYTZ(TZProvider):
         )
         return cls()
 
-    def timezone(self, name: str) -> Optional[tzinfo]:
+    def timezone(self, name: str) -> tzinfo | None:
         """Return a timezone with a name or None if we cannot find it."""
         try:
             return pytz.timezone(name)

src/icalendar/timezone/tzid.py
@@ -10,7 +10,7 @@ from __future__ import annotations
 from collections import defaultdict
 from datetime import date, time, timezone
 from pathlib import Path
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 
 from dateutil.tz import tz
 
@@ -21,13 +21,13 @@ if TYPE_CHECKING:
     from datetime import datetime, tzinfo
 
 DATEUTIL_UTC = tz.gettz("UTC")
-DATEUTIL_UTC_PATH: Optional[str] = getattr(DATEUTIL_UTC, "_filename", None)
+DATEUTIL_UTC_PATH: str | None = getattr(DATEUTIL_UTC, "_filename", None)
 DATEUTIL_ZONEINFO_PATH = (
     None if DATEUTIL_UTC_PATH is None else Path(DATEUTIL_UTC_PATH).parent
 )
 
 
-def tzids_from_tzinfo(tzinfo: Optional[tzinfo]) -> tuple[str]:
+def tzids_from_tzinfo(tzinfo: tzinfo | None) -> tuple[str]:
     """Get several timezone ids if we can identify the timezone.
 
     >>> import zoneinfo
@@ -65,7 +65,7 @@ def tzids_from_tzinfo(tzinfo: Optional[tzinfo]) -> tuple[str]:
     return ()
 
 
-def tzid_from_tzinfo(tzinfo: Optional[tzinfo]) -> Optional[str]:
+def tzid_from_tzinfo(tzinfo: tzinfo | None) -> str | None:
     """Retrieve the timezone id from the tzinfo object.
 
     Some timezones are equivalent.
@@ -79,7 +79,7 @@ def tzid_from_tzinfo(tzinfo: Optional[tzinfo]) -> Optional[str]:
     return tzids[0]
 
 
-def tzid_from_dt(dt: datetime) -> Optional[str]:
+def tzid_from_dt(dt: datetime) -> str | None:
     """Retrieve the timezone id from the datetime object."""
     tzid = tzid_from_tzinfo(dt.tzinfo)
     if tzid is None:

src/icalendar/timezone/tzp.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Optional, Union
+from typing import TYPE_CHECKING
 
 from icalendar.tools import to_datetime
 
@@ -27,19 +27,19 @@ class TZP:
     All of icalendar will then use this timezone implementation.
     """
 
-    def __init__(self, provider: Union[str, TZProvider] = DEFAULT_TIMEZONE_PROVIDER):
+    def __init__(self, provider: str | TZProvider = DEFAULT_TIMEZONE_PROVIDER):
         """Create a new timezone implementation proxy."""
         self.use(provider)
 
     def use_pytz(self) -> None:
         """Use pytz as the timezone provider."""
-        from .pytz import PYTZ
+        from .pytz import PYTZ  # noqa: PLC0415, RUF100
 
         self._use(PYTZ())
 
     def use_zoneinfo(self) -> None:
         """Use zoneinfo as the timezone provider."""
-        from .zoneinfo import ZONEINFO
+        from .zoneinfo import ZONEINFO  # noqa: PLC0415, RUF100
 
         self._use(ZONEINFO())
 
@@ -48,7 +48,7 @@ class TZP:
         self.__tz_cache = {}
         self.__provider = provider
 
-    def use(self, provider: Union[str, TZProvider]):
+    def use(self, provider: str | TZProvider):
         """Switch to a different timezone provider."""
         if isinstance(provider, str):
             use_provider = getattr(self, f"use_{provider}", None)
@@ -72,7 +72,7 @@ class TZP:
         return self.__provider.localize_utc(to_datetime(dt))
 
     def localize(
-        self, dt: datetime.date, tz: Union[datetime.tzinfo, str, None]
+        self, dt: datetime.date, tz: datetime.tzinfo | str | None
     ) -> datetime.datetime:
         """Localize a datetime to a timezone."""
         if isinstance(tz, str):
@@ -116,7 +116,7 @@ class TZP:
         """
         return tzid.strip("/")
 
-    def timezone(self, tz_id: str) -> Optional[datetime.tzinfo]:
+    def timezone(self, tz_id: str) -> datetime.tzinfo | None:
         """Return a timezone with an id or None if we cannot find it."""
         _unclean_id = tz_id
         tz_id = self.clean_timezone_id(tz_id)

src/icalendar/timezone/windows_to_olson.py
@@ -6,7 +6,7 @@ for this mapping is also available at the unicode consortium [1].
 
 
 [0] https://www.unicode.org/cldr/cldr-aux/charts/29/supplemental/zone_tzid.html
-[1] https://cldr.unicode.org/development/development-process/design-proposals/extended-windows-olson-zid-mapping  # noqa
+[1] https://cldr.unicode.org/development/development-process/design-proposals/extended-windows-olson-zid-mapping
 """
 
 WINDOWS_TO_OLSON = {

src/icalendar/timezone/zoneinfo.py
@@ -6,6 +6,7 @@ import copy
 import copyreg
 import functools
 import threading
+import zoneinfo
 from datetime import datetime, tzinfo
 from io import StringIO
 from typing import TYPE_CHECKING
@@ -14,7 +15,6 @@ from dateutil.rrule import rrule, rruleset
 from dateutil.tz import tzical
 from dateutil.tz.tz import _tzicalvtz
 
-import zoneinfo
 from icalendar.tools import is_date, to_datetime
 
 from .provider import TZProvider
@@ -101,7 +101,7 @@ class ZONEINFO(TZProvider):
                         sub.pop(attr)
             for sub in tz.subcomponents:
                 start: vDDDTypes = sub.get("DTSTART")
-                if start and hasattr(start, 'dt') and is_date(start.dt):
+                if start and hasattr(start, "dt") and is_date(start.dt):
                     # ValueError: Unsupported DTSTART param in VTIMEZONE: VALUE=DATE
                     sub.DTSTART = to_datetime(start.dt)
             return self._create_timezone(tz)

commit 51f05ddf297bcd61083c54bc322e9f586cda24b4

Fix equivalent_timezone_ids script
This was broken in 0c4ea18bdd0f37fe46aea43a3bda96590d4ee3c6 as the
pprint call which produces the code was removed.

src/icalendar/timezone/equivalent_timezone_ids.py
@@ -21,6 +21,7 @@ from __future__ import annotations
 from collections import defaultdict
 from datetime import datetime, timedelta, tzinfo
 from pathlib import Path
+from pprint import pprint
 from typing import TYPE_CHECKING, NamedTuple
 from zoneinfo import ZoneInfo, available_timezones
 
@@ -123,6 +124,7 @@ def main(
         )
         f.write("import datetime\n\n")
         f.write("\nlookup = ")
+        pprint(lookup, stream=f)
         f.write("\n\n__all__ = ['lookup']\n")
 
     return lookup