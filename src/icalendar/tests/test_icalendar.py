d04ce5412122e86833a4137225308c21ca4a225c
IoannaGiag <t8230022@aueb.gr>
2026-05-24 03:10:11 +0300

    deprecate foldline for icalendar 8 (#1011) (#1391)
    * deprecate foldline for icalendar 8 (#1011)
    
    * Apply ruff formatting fixes
    
    * Apply ruff fixes
    
    * fix: correct deprecation warning to use public function name

--- src/icalendar/tests/test_icalendar.py
+++ src/icalendar/tests/test_icalendar.py
@@ -6,8 +6,8 @@ from icalendar.parser import (
     Contentline,
     Contentlines,
     Parameters,
+    _foldline,
     dquote,
-    foldline,
     q_join,
     q_split,
 )
@@ -243,9 +243,9 @@ class IcalendarTestCase(unittest.TestCase):
         )
 
     def test_fold_line(self):
-        assert foldline("foo") == "foo"
+        assert _foldline("foo") == "foo"
         assert (
-            foldline(
+            _foldline(
                 "Lorem ipsum dolor sit amet, consectetur adipiscing "
                 "elit. Vestibulum convallis imperdiet dui posuere.",
             )
@@ -257,11 +257,11 @@ class IcalendarTestCase(unittest.TestCase):
         # at least just but bytes in there
         # porting it to "run" under python 2 & 3 makes it not much better
         with pytest.raises(AssertionError):
-            foldline("привет".encode(), limit=3)
+            _foldline("привет".encode(), limit=3)
 
-        assert foldline("foobar", limit=4) == "foo\r\n bar"
+        assert _foldline("foobar", limit=4) == "foo\r\n bar"
         assert (
-            foldline(
+            _foldline(
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
                 ". Vestibulum convallis imperdiet dui posuere.",
             )
@@ -269,7 +269,7 @@ class IcalendarTestCase(unittest.TestCase):
             + " Vestibulum conval\r\n lis imperdiet dui posuere."
         )
         assert (
-            foldline("DESCRIPTION:АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ")
+            _foldline("DESCRIPTION:АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ")
             == "DESCRIPTION:АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭ\r\n ЮЯ"
         )
 
