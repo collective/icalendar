49b87a97272857ade7a32a3bdaddfca8488f33ba
Alhuda Khan <al.hudz.k@gmail.com>
2026-07-07 15:56:33 +0530

    share the CR/LF test parameters and cover jCal cal-address values

--- src/icalendar/tests/prop/test_crlf_in_values.py
+++ src/icalendar/tests/prop/test_crlf_in_values.py
@@ -14,29 +14,38 @@ from icalendar.prop import vUri
 from icalendar.prop.inline import vInline
 from icalendar.prop.xml_reference import vXmlReference
 
+VERBATIM_VALUE_TYPES = [vUri, vCalAddress, vInline, vXmlReference]
 
-@pytest.mark.parametrize("cls", [vUri, vCalAddress, vInline, vXmlReference])
-@pytest.mark.parametrize(
-    "value",
-    [
-        "mailto:a@b.com\rINJECTED:evil",
-        "mailto:a@b.com\nINJECTED:evil",
-        "http://e.com/a\r\nX-EVIL:1",
-        "trailing\r",
-    ],
-)
+CRLF_INJECTION_VALUES = [
+    "mailto:a@b.com\rINJECTED:evil",
+    "mailto:a@b.com\nINJECTED:evil",
+    "http://e.com/a\r\nX-EVIL:1",
+    "trailing\r",
+]
+
+JCAL_PROPERTIES = [
+    ("url", "uri"),
+    ("attendee", "cal-address"),
+    ("organizer", "cal-address"),
+]
+
+
+@pytest.mark.parametrize("cls", VERBATIM_VALUE_TYPES)
+@pytest.mark.parametrize("value", CRLF_INJECTION_VALUES)
 def test_value_rejects_cr_and_lf(cls, value):
     with pytest.raises(ValueError):
         cls.from_ical(value)
 
 
-@pytest.mark.parametrize("cls", [vUri, vCalAddress, vInline, vXmlReference])
+@pytest.mark.parametrize("cls", VERBATIM_VALUE_TYPES)
 def test_valid_values_still_parse(cls):
     assert cls.from_ical("http://example.com/my-report.txt")
 
 
-def test_jcal_uri_value_with_cr_is_rejected():
-    """A lone CR from untrusted jCal must not reach the serialised output."""
+@pytest.mark.parametrize(("prop", "jcal_type"), JCAL_PROPERTIES)
+@pytest.mark.parametrize("value", CRLF_INJECTION_VALUES)
+def test_jcal_value_with_cr_or_lf_is_rejected(prop, jcal_type, value):
+    """A CR or LF from untrusted jCal must not reach the serialised output."""
     jcal = [
         "vcalendar",
         [["version", {}, "text", "2.0"], ["prodid", {}, "text", "x"]],
@@ -45,7 +54,7 @@ def test_jcal_uri_value_with_cr_is_rejected():
                 "vevent",
                 [
                     ["uid", {}, "text", "1"],
-                    ["url", {}, "uri", "http://e.com/a\rX-EVIL:1"],
+                    [prop, {}, jcal_type, value],
                 ],
                 [],
             ]
