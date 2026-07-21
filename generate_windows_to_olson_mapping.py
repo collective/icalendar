d1f24de361c6b035d9a637571331dec79625ffbd
Patrick <patrickswedish@users.noreply.github.com>
2026-07-22 00:33:31 +0200

    Address changelog attribution and formatting

--- generate_windows_to_olson_mapping.py
+++ generate_windows_to_olson_mapping.py
@@ -14,9 +14,7 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 
 CLDR_PATH = "common/supplemental/windowsZones.xml"
-commits_url = (
-    f"https://api.github.com/repos/unicode-org/cldr/commits?path={CLDR_PATH}&sha=main&per_page=1"
-)
+commits_url = f"https://api.github.com/repos/unicode-org/cldr/commits?path={CLDR_PATH}&sha=main&per_page=1"
 with req.urlopen(commits_url) as response:  # noqa: S310
     version = json.load(response)[0]["sha"]
 
