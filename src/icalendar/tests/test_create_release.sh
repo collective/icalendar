657cd2dc9fe5d14ebb85efd36fec4bd4a9f916a0
Nicco Kunzmann <niccokunzmann@rambler.ru>
2026-08-18 13:43:46 +0100

    Improve tests

--- src/icalendar/tests/test_create_release.sh
+++ src/icalendar/tests/test_create_release.sh
@@ -25,19 +25,21 @@ if ! [ -f "$archive" ]; then
   exit 1
 fi
 
-if tar -tf "$archive" | grep -q 'fuzzing/'; then
+FILES="`tar -tf \"$archive\" | grep -o '/.*'`"
+
+if echo "$FILES" | grep -q '^/src/icalendar/fuzzing'; then
   echo "ERROR: Fuzzing files are included in the release."
   echo "       See https://github.com/collective/icalendar/pull/569"
   exit 1
 fi
 
-if ! tar -tf "$archive" | grep -q '/docs/'; then
+if ! echo "$FILES" | grep -q '^/docs/'; then
   echo "ERROR: The documentation is not included in the release, but should be."
   echo "       See https://github.com/collective/icalendar/issues/712"
   exit 1
 fi
 
-if ! tar -tf "$archive" | grep -q '/funding.json'; then
+if ! echo "$FILES" | grep -q '/funding.json$'; then
   echo "ERROR: Funding files need to be included in the release files."
   echo "       See https://github.com/collective/icalendar/issues/1493"
   exit 1
