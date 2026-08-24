9479451c7850b71969fd25b10daf39989f971d78
Vedant Patel <vedanthp1@gmail.com>
2026-08-24 14:47:28 -0700

    Deleted duplicate action check.

--- src/icalendar/cal/alarm.py
+++ src/icalendar/cal/alarm.py
@@ -421,11 +421,6 @@ class Alarm(Component):
             refids=refids,
             concepts=concepts,
         )
-        if action == "AUDIO" and isinstance(attachments, list) and len(attachments) > 1:
-            raise InvalidCalendar(
-                "An AUDIO alarm must not contain more than one attachment.\n"
-                f"Alarm has {len(attachments)} attachments."
-            )
         if action is not None:
             alarm.ACTION = action
         alarm.attachments = attachments
