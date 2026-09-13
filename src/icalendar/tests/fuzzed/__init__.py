0e9c941e68e0555cad192ed6f08973dee48e6cf5
Jonas Eckert <j.eckert@ostfalia.de>
2026-09-13 12:28:44 +0200

    Derive version name from caller

--- src/icalendar/tests/fuzzed/__init__.py
+++ src/icalendar/tests/fuzzed/__init__.py
@@ -5,6 +5,8 @@ These test cases reproduce the failure.
 Some more tests can be added to make sure that the behavior works properly.
 """
 
+import inspect
+
 _value_error_matches = [
     "component",
     "parse",
@@ -40,7 +42,7 @@ _value_error_matches = [
 
 
 def format_fuzz_log(
-    version: str, from_ical, multiple: bool, should_walk: bool, calendar_string: str | bytes
+    from_ical, multiple: bool, should_walk: bool, calendar_string: str | bytes
 ) -> str:
     """Format the log entry for fuzzed test case extraction.
 
@@ -48,17 +50,28 @@ def format_fuzz_log(
     """
     import base64
 
-    # print the ICS file for the test case extraction
-    # see https://stackoverflow.com/a/27367173/1320237
-    encoded = base64.b64encode(
-        calendar_string.encode("UTF-8", "surrogateescape")
-        if isinstance(calendar_string, str)
-        else calendar_string
-    ).decode("ASCII")
+    try:
+        # print the ICS file for the test case extraction
+        # see https://stackoverflow.com/a/27367173/1320237
+        encoded = base64.b64encode(
+            calendar_string.encode("UTF-8", "surrogateescape")
+            if isinstance(calendar_string, str)
+            else calendar_string
+        ).decode("ASCII")
+    except UnicodeEncodeError as e:
+        encoded = str(e)
     return (
-        f"{version} {from_ical.__qualname__} multiple={multiple} "
+        f"{_get_name_of_caller()} {from_ical.__qualname__} multiple={multiple} "
         f"should_walk={should_walk} {encoded}"
     )
+
+
+def _get_name_of_caller(skip: int = 0) -> str:
+    stack = inspect.stack()
+    relevant_index = skip + 2  # ignore own and caller frame
+    if relevant_index < len(stack) and (frame := stack[relevant_index]):
+        return frame.function
+    return "<unknown source>"
 
 
 def fuzz_v1_calendar(
@@ -68,7 +81,7 @@ def fuzz_v1_calendar(
 
     The calendar_string is a fuzzed input.
     """
-    print(format_fuzz_log("v1", from_ical, multiple, should_walk, calendar_string))
+    print(format_fuzz_log(from_ical, multiple, should_walk, calendar_string))
     try:
         cal = from_ical(calendar_string, multiple=multiple)
 
