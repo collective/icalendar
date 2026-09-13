7c01340343afd23377d79a552183339dd4ed5b4a
Jonas Eckert <j.eckert@ostfalia.de>
2026-09-13 12:31:03 +0200

    Fix ruff error

--- src/icalendar/tests/fuzzed/test_fuzzed_calendars.py
+++ src/icalendar/tests/fuzzed/test_fuzzed_calendars.py
@@ -41,6 +41,8 @@ def test_format_fuzz_log():
         should_walk=True,
         calendar_string=content,
     )
-    assert log.startswith("test_format_fuzz_log Calendar.from_ical multiple=False should_walk=True ")
+    assert log.startswith(
+        "test_format_fuzz_log Calendar.from_ical multiple=False should_walk=True "
+    )
     encoded = log.split()[-1]
     assert base64.b64decode(encoded).decode("utf-8") == content
