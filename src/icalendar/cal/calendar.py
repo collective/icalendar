31906f4fa9e5486223a25fefb7225b7e7512beca
Mohammed Ahmed <224961588+Mohammed-Ahmed7@users.noreply.github.com>
2026-09-13 13:11:51 +0200

    Update src/icalendar/cal/calendar.py
    Co-authored-by: Steve Piercy <web@stevepiercy.com>

--- src/icalendar/cal/calendar.py
+++ src/icalendar/cal/calendar.py
@@ -170,7 +170,7 @@ class Calendar(Component):
 
     @property
     def availabilities(self) -> list[Availability]:
-        """All :class:`Availability <icalendar.cal.availability.Availability>` components in the calendar.
+        """All :class:`~icalendar.cal.availability.Availability` components in the calendar.
 
         This is a shortcut to get all availabilities.
         Modifications do not change the calendar.
